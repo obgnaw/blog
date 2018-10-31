e2875b41dc29230cd7971ac54d54c886b6e843d5
wangbo <wangbo@koal.com>
2018-10-31 16:16:48 +0800

    change

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,19 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, abort, flash
+from flask_login import login_required, current_user
 from . import main
+from ..models import Role, User, Post
+from .forms import PostForm
 
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    form = PostForm()
+    if form.validate_on_submit():
+        # post = Post(body=form.body.data,
+        #             author=current_user._get_current_object())
+        # db.session.add(post)
+        # db.session.commit()
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
