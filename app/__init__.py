b1b96e286e806813fbf30ea0a070a1fff82c1ad2
wangbo <wangbo@koal.com>
2018-10-31 09:52:19 +0800

    peewee

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from config import config
-
+from peewee import SqliteDatabase
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
-db = SQLAlchemy()
-
+# db = SQLAlchemy()
+db = SqliteDatabase('data-dev.sqlite', **{})
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,7 +19,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
-    db.init_app(app)
+    # db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
